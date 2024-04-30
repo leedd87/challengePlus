44132fd20d2a6cc279acc24221e8ffd13c580293
dlee2011 <dlee@itcrowdarg.com>
2024-04-30 08:59:31 -0300

    project initial setup

---
+++ src/navigation/MainStackNavigator/MainStackNavigator.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import {createStackNavigator} from '@react-navigation/stack';
+import {DetailsScreen, HomeScreen} from '../../screens';
+
+const Stack = createStackNavigator();
+
+export const MainStackNavigator = () => {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name="Home" component={HomeScreen} />
+      <Stack.Screen name="Details" component={DetailsScreen} />
+    </Stack.Navigator>
+  );
+};
