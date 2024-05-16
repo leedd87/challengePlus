4abeebc9d585883406febc92d313dc4087c2fd97
leedd87 <leedav87@gmail.com>
2024-05-16 12:10:12 -0300

    refactoring back btn

--- src/common/layout/Header/components/BackBtn/styles.ts
+++ src/common/layout/Header/components/BackBtn/styles.ts
@@ -1,12 +1,14 @@
 import {StyleSheet} from 'react-native';
+import {colors} from '../../../../../theme/colors';
 
 export const styles = StyleSheet.create({
   container: {
-    backgroundColor: 'red',
+    backgroundColor: colors.main,
     position: 'absolute',
     left: 15,
     padding: 8,
     paddingHorizontal: 12,
     borderRadius: 16,
+    borderWidth: 1,
   },
 });
